933ca2f33daedae6f3c57f705221f2b3b1826aba
deboguer-jng <deboguer.jslang@gmail.com>
2023-07-11 09:21:12 -0500

    feat: update hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -47,6 +47,33 @@ module.exports = {
 					},
 				},
 			},
+			{
+				version: "0.8.4",
+				settings: {
+					optimizer: {
+						enabled: true,
+						runs: 200,
+					},
+				},
+			},
+			{
+				version: "0.7.6",
+				settings: {
+					optimizer: {
+						enabled: true,
+						runs: 200,
+					},
+				},
+			},
+			{
+				version: "0.8.12",
+				settings: {
+					optimizer: {
+						enabled: true,
+						runs: 200,
+					},
+				},
+			},
 		],
 	},
 	networks: {
